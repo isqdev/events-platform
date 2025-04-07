2fa6a0d8527996b310f0d94a4897acc67339e33f
Isaque Dias <isaque.brain@gmail.com>
2025-04-07 10:51:26 -0300

    feat: implementing test

--- src/model/entities/eventos.py
+++ src/model/entities/eventos.py
@@ -4,5 +4,5 @@ from sqlalchemy import Column, String, Integer
 class Eventos(Base):
     __tablename__ = "Eventos"
 
-    id = Column(Integer, primary_key=True, autoIncrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String, nullable=False)
